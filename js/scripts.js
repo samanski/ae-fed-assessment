68a7b187baf1ee431e7a1b43f1dd62362d0b22a8
samanski <samfizor@gmail.com>
2017-04-12 21:09:42 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,6 @@ $(function () {
         speed: 1200
     });
 
-    if ($(window).width() <= 1024) {
-        $('#menu-toggle').click(function () {
-            $(this).toggleClass('expanded').siblings('div').slideToggle();
-        });
-
-        $('#accordion').find('.accordion-toggle').click(function () {
-            $(this).next().slideToggle('fast');
-            $(".accordion-content").not($(this).next()).slideUp('fast');
-
-        });
-    }
-
     function setWindowHeight() {
         var windowHeight = window.innerHeight;
         document.body.style.height = windowHeight + "px";
